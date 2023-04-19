b24b1d60676347c8d785efcf94273fbfd61e48af
Olexandr Shalakhin <olexandr@shalakhin.com>
2023-04-19 17:19:27 +0300

    update for the first release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as f:
     description = f.read()
 
 setuptools.setup(
-    name="elliptic-sdk",
+    name="elliptic",
     version="0.0.1",
     author="Olexandr Shalakhin",
     author_email="olexandr@shalakhin.com",
@@ -12,6 +12,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.10',
