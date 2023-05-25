1bbce98123d43b30195b40ba9a1bd3d02253599d
Andrei Mukhametvaleev <andrei.mukhametvaleev@gmail.com>
2023-05-25 14:46:52 +0400

    refactor(settings): add `env_prefix`

--- elliptic_sdk/settings.py
+++ elliptic_sdk/settings.py
@@ -1,18 +1,19 @@
 """Elliptic SDK elliptic_sdk/settings.py."""
-from pydantic import BaseSettings, Field
+from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
     """Settings."""
 
-    api_key: str = Field('', env='ELLIPTIC_API_KEY')
-    api_secret: str = Field('', env='ELLIPTIC_API_SECRET')
+    api_key: str
+    api_secret: str
 
     class Config:
         """Config."""
 
         env_file = '.env'
         env_file_encoding = 'utf-8'
+        env_prefix = 'ELLIPTIC_'
 
 
 settings = Settings()
